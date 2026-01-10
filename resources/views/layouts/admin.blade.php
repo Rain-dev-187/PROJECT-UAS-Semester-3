045a48c621b7c7bc53eb3bb40ffbbe8fc622e38b
Rain-dev-187 <ahmadsahrul1887@gmail.com>
2026-01-10 17:59:20 +0700

    update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -192,7 +192,6 @@
         <!-- Main Footer -->
         <footer class="main-footer">
             <div class="float-right d-none d-sm-inline">
-                Anything you want
             </div>
             <strong>Copyright &copy; {{ date('Y') }} <a href="#">PULSA</a>.</strong> All rights reserved.
         </footer>
